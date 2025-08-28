42685154ccf739d9ee5bd3adc6d3b76e27025550
Mryagci <31544170+Mryagci@users.noreply.github.com>
2025-08-28 13:35:43 +0300

    Update route.ts

--- src/app/api/payment/callback/route.ts
+++ src/app/api/payment/callback/route.ts
@@ -1,9 +1,10 @@
-export const dynamic = 'force-dynamic';
-export const revalidate = 0;
-export const runtime = 'nodejs';
 import { NextRequest, NextResponse } from 'next/server'
 import { createClient } from '@supabase/supabase-js'
 import crypto from 'crypto'
+
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
+export const runtime = 'nodejs';
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
 const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE_KEY!
